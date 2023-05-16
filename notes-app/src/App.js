48a39d7ed2f53b0e8917fc2caeb262b4f3347a9a
Sedakat <sedakatekmekci16@gmail.com>
2023-05-16 18:49:50 +0300

    Update

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,10 +1,18 @@
 
 import './App.css';
+import bootstrap from "bootstrap";
 
 function App() {
   return (
     <div className="App">
-
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <button className='btn btn-dnager
+      '></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
