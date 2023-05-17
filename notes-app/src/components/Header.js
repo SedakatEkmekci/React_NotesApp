47213e99a5d34205e2f650614c06216b540f6d69
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 11:30:38 +0300

    Create Header.js

---
+++ notes-app/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Form } from 'react-bootstrap';
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Notes App</h1>
+            <Form />
+        </header>
+    )
+}
+
+export default Header;
