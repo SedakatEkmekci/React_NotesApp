d0378236c028ed7bcc74c0faf5846db2df4529e1
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 12:06:07 +0300

    Update App.js

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
+
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <>
+      <section className='notesApp'>
+        <Header />
+      </section>
+      <Footer />
+    </>
   );
 }
 
