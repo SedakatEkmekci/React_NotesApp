9d021b1181fa11b543ca2df7137822e381c9ac9e
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 12:24:48 +0300

    Create Notes.js

---
+++ notes-app/src/components/Notes.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Notes() {
+    return (
+        <ul className="note-list">
+            <li className='note'> Test
+                <button className="note__delete-button" type="button" > X </button>
+            </li>
+        </ul>
+
+
+    )
+}
+
+export default Notes
