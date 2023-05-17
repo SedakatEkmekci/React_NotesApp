59c2eff498794c718f94892e718acbd5446dff82
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 11:50:25 +0300

    Update store.js

--- notes-app/src/redux/store.js
+++ notes-app/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import notesSlice from "./notes/notesSlice";
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        notes: notesSlice,
+    },
 });
