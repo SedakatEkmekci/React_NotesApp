936bc796c0c5887587c828f99d8ea34b8d6501f1
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 12:30:20 +0300

    Update notesSlice.js

--- notes-app/src/redux/notes/notesSlice.js
+++ notes-app/src/redux/notes/notesSlice.js
@@ -3,9 +3,27 @@ import { createSlice } from "@reduxjs/toolkit";
 export const notesSlice = createSlice({
     name: 'notes',
     initialState: {
-        items: [],
+        items: [
+            {
+                id: 1,
+                title: 'Note 1',
+                color: 'pink'
+            },
+            {
+                id: 2,
+                title: 'Note 2',
+                color: 'yellow'
+            },
+            {
+                id: 3,
+                title: 'Note 3',
+                color: 'blue'
+            }
+
+        ],
     },
     reducers: {},
 });
+export const selectNotes = state => state.notes.items;
 
 export default notesSlice.reducer;
