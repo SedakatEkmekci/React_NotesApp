2f5f6426b9d48564ddcf974d6c860b6f39c18e15
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 12:39:38 +0300

    Update Notes.js

--- notes-app/src/components/Notes.js
+++ notes-app/src/components/Notes.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectNotes } from '../redux/notes/notesSlice';
+
+
 
 function Notes() {
+
+    const items = useSelector(selectNotes);
+    // console.log(items);
     return (
         <ul className="note-list">
-            <li className='note'> Test
-                <button className="note__delete-button" type="button" > X </button>
-            </li>
+
+            {
+                items.map((item) => (
+
+                    <li key={item.id} >
+                        <div className='note'>
+                            <label>{item.title}</label>
+                            <button className="destroy"></button>
+
+                        </div>
+                    </li>
+
+
+                ))
+            }
         </ul>
 
 
