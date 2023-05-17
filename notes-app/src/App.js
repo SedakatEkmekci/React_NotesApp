3bf38730f4213b45fb81b0bce3a200ac7f3c29eb
Sedakat <sedakatekmekci16@gmail.com>
2023-05-17 11:29:58 +0300

    Update App.js

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -1,22 +1,13 @@
-
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Alert } from 'react-bootstrap';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6">
-            <button className='btn btn-danger'>Gönder</button>
-            <Alert variant={'danger'}>
-              This is a alert—check it out!
-            </Alert>
+      <Header>
 
-          </div>
-        </div>
-      </div>
+      </Header>
     </div>
   );
 }
