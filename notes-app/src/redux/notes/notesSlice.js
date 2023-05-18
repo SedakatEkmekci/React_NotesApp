b1572c1345fafa7b04842e2df49c3f5118964b92
Sedakat <sedakatekmekci16@gmail.com>
2023-05-18 20:20:30 +0300

    Update notesSlice.js

--- notes-app/src/redux/notes/notesSlice.js
+++ notes-app/src/redux/notes/notesSlice.js
@@ -9,16 +9,7 @@ export const notesSlice = createSlice({
                 title: 'Note 1',
 
             },
-            {
-                id: 2,
-                title: 'Note 2',
 
-            },
-            {
-                id: 3,
-                title: 'Note 3',
-
-            }
 
         ],
     },
