81e8479e69d463cd68632d35110f5f72930812b6
Sedakat <sedakatekmekci16@gmail.com>
2023-05-18 20:44:33 +0300

    Update NoteEditor.js
    Test

--- notes-app/src/components/NoteEditor.js
+++ notes-app/src/components/NoteEditor.js
@@ -4,6 +4,7 @@ import { addNote } from '../redux/notes/notesSlice';
 
 function NoteEditor() {
     const [title, setTitle] = useState('');
+
     const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
@@ -15,6 +16,7 @@ function NoteEditor() {
 
     return (
 
+
         <form className="note-editor" onSubmit={handleSubmit} >
             <input
                 placeholder="Enter your note here..."
@@ -25,6 +27,7 @@ function NoteEditor() {
 
             />
             <div className="color-picker">
+
                 <input type="radio" name="color-pick" value="#F06292" id="color1" />
                 <label htmlFor="color1" style={{ backgroundColor: "#F06292" }}></label>
                 <input type="radio" name="color-pick" value="#BA68C8" id="color2" />
@@ -35,7 +38,11 @@ function NoteEditor() {
                 <label htmlFor="color4" style={{ backgroundColor: "#4FC3F7" }}></label>
                 <input type="radio" name="color-pick" value="#AED581" id="color5" />
                 <label htmlFor="color5" style={{ backgroundColor: "#AED581" }}></label>
+
+
             </div>
+
+
             <button className="add-button"  >Add</button>
         </form>
 
