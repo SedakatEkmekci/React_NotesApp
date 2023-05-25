50229c229b90478e205d650cc4a573ceb6b608a1
Sedakat <sedakatekmekci16@gmail.com>
2023-05-25 22:30:58 +0300

    Update Search.js

--- notes-app/src/components/Search.js
+++ notes-app/src/components/Search.js
@@ -1,16 +1,28 @@
 import { React } from 'react';
+import { useDispatch } from 'react-redux';
+import { searchNote } from '../redux/notes/notesSlice';
 
 
 
 
 function Search() {
+    const dispatch = useDispatch();
+
+    const handleSearch = (e) => {
+        dispatch(searchNote({ search: e.target.value }));
+    }
 
 
     return (
 
-        <input className='search-input' type='search' placeholder='Search..' />
+        <input
+            className='search-input'
+            type='search'
+            placeholder='Search..'
+            onChange={(e) => handleSearch(e)}
+        />
 
-    )
+    );
 }
 
 export default Search;
