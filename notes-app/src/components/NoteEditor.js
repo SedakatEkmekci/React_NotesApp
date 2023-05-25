29a9b27d37ba89cd1b745ea97ae482f6525065ac
Sedakat <sedakatekmekci16@gmail.com>
2023-05-25 20:31:23 +0300

    Update NoteEditor.js

--- notes-app/src/components/NoteEditor.js
+++ notes-app/src/components/NoteEditor.js
@@ -1,44 +1,45 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addNote } from '../redux/notes/notesSlice';
+import { nanoid } from '@reduxjs/toolkit';
 
 function NoteEditor() {
-    const [title, setTitle] = useState('');
+    const [text, settext] = useState('');
+    const [color, setColor] = useState('#FFF');
 
     const dispatch = useDispatch();
 
     const handleSubmit = (e) => {
-        if (!title) return;
         e.preventDefault();
-        dispatch(addNote({ title }));
-        setTitle('');
+        if (!text) return;
+
+        dispatch(addNote({
+            id: nanoid(),
+            text: text,
+            color: color
+        }));
+        settext('');
+
     };
 
     return (
-
-
         <form className="note-editor" onSubmit={handleSubmit} >
-            <input
+            <textarea className="textarea"
+                id="note"
                 placeholder="Enter your note here..."
-                className="textarea"
-                autoFocus
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+                value={text}
+                onChange={(e) => settext(e.target.value)}
+            >
 
-            />
+            </textarea>
+
             <div className="color-picker">
 
-                <input type="radio" name="color-pick" value="#F06292" id="color1" />
-                <label htmlFor="color1" style={{ backgroundColor: "#F06292" }}></label>
-                <input type="radio" name="color-pick" value="#BA68C8" id="color2" />
-                <label htmlFor="color2" style={{ backgroundColor: "#BA68C8" }}></label>
-                <input type="radio" name="color-pick" value="#FFD54F" id="color3" />
-                <label htmlFor="color3" style={{ backgroundColor: "#FFD54F" }}></label>
-                <input type="radio" name="color-pick" value="#4FC3F7" id="color4" />
-                <label htmlFor="color4" style={{ backgroundColor: "#4FC3F7" }}></label>
-                <input type="radio" name="color-pick" value="#AED581" id="color5" />
-                <label htmlFor="color5" style={{ backgroundColor: "#AED581" }}></label>
-
+                <button id='#F06292' style={{ backgroundColor: '#F06292' }} type='button' onClick={(e) => setColor(e.target.id)} className="color-btn" />
+                <button id='#BA68C8' style={{ backgroundColor: '#BA68C8' }} type='button' onClick={(e) => setColor(e.target.id)} className="color-btn" />
+                <button id='#FFD54F' style={{ backgroundColor: '#FFD54F' }} type='button' onClick={(e) => setColor(e.target.id)} className="color-btn" />
+                <button id='#4FC3F7' style={{ backgroundColor: '#4FC3F7' }} type='button' onClick={(e) => setColor(e.target.id)} className="color-btn" />
+                <button id='#AED581' style={{ backgroundColor: '#AED581' }} type='radio' onClick={(e) => setColor(e.target.id)} className="color-btn" />
 
             </div>
 
@@ -47,6 +48,6 @@ function NoteEditor() {
         </form>
 
     )
-}
+};
 
 export default NoteEditor
