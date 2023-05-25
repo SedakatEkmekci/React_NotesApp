72eaaa205e7b90e8712c625193f5c66788b000dc
Sedakat <sedakatekmekci16@gmail.com>
2023-05-25 22:30:55 +0300

    Update Notes.js

--- notes-app/src/components/Notes.js
+++ notes-app/src/components/Notes.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { selectNotes } from '../redux/notes/notesSlice';
+import { selectNotes, getFilteredNotes } from '../redux/notes/notesSlice';
 
 
 
 function Notes() {
 
-    const items = useSelector(selectNotes);
+    const notes = useSelector(getFilteredNotes);
+    console.log(notes);
 
 
     return (
 
         <ul className="note-list">
+            {notes.length > 0 ? (
+                notes.map((note) => (
 
-            {
-                items.map((item) => (
-
-                    <li key={item.id}  >
-                        <div className='note' style={{ backgroundColor: item.color }}>
-                            <label >{item.text}</label>
+                    <li key={note.id}  >
+                        <div className='note' style={{ backgroundColor: note.color }}>
+                            <label >{note.text}</label>
                         </div>
                     </li>
-
-
                 ))
-            }
+            ) : (
+                <li>
+                    <div className='note' style={{ backgroundColor: '#FFF' }}>
+                        <label >There is no note here!</label>
+                    </div>
+                </li>
+            )}
         </ul>
 
-
-
-    )
+    );
 }
 
 export default Notes
