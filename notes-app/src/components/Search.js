5bc7b8a8c8d0a1733b0e40db052e132e9e0ce256
Sedakat <sedakatekmekci16@gmail.com>
2023-05-26 16:26:26 +0300

    Update Search.js

--- notes-app/src/components/Search.js
+++ notes-app/src/components/Search.js
@@ -1,28 +1,58 @@
 import { React } from 'react';
-import { useDispatch } from 'react-redux';
-import { searchNote } from '../redux/notes/notesSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { filterNote, searchNote, deleteNote, filteredNote } from '../redux/notes/notesSlice';
 
 
 
 
 function Search() {
     const dispatch = useDispatch();
-
-    const handleSearch = (e) => {
-        dispatch(searchNote({ search: e.target.value }));
-    }
-
+    const notes = useSelector(searchNote);
+    const filtered = useSelector(filteredNote);
 
     return (
+        <>
+            <input
+                className='search-input'
+                type='search'
+                placeholder='Search..'
+                value={notes}
+                onChange={(e) => dispatch(filterNote(e.target.value))}
+            />
+            <br /><br />
+            <div className="note-list">
+                {
+                    notes.length > 0 ? (
+                        filtered.map(note => (
+                            <div key={note.id} className="note" style={{ backgroundColor: `${note.color}` }}>
 
-        <input
-            className='search-input'
-            type='search'
-            placeholder='Search..'
-            onChange={(e) => handleSearch(e)}
-        />
+                                <label >{note.text}</label>
+                                <button className="delete-btn" onClick={() => dispatch(deleteNote(note.id))}><i className="fa-solid fa-trash"></i></button>
+                            </div>
+                        ))
+                    ) : (
 
-    );
-}
+                        <div className='note' style={{ backgroundColor: '#FFF' }}>
+                            <label >There is no note here!</label>
+                        </div>
+
+                    )
+                }
+
+            </div>
+
+        </>
+    )
+
+};
+
+
+
+
+
+
+
+
+
 
 export default Search;
